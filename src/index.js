f28e3f6f4bd6be6bd5408006deeb645edbe9604f
Vladimir Abramovich <abramingys@gmail.com>
2024-07-29 18:11:31 +0300

    added: npm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  return +Array.from(num.toString()).reverse().join('');
-}
+module.exports = function reverse(n) {
+  let reversedStr = Math.abs(n).toString().split('').reverse().join('');
+
+  let reversedNumber = parseInt(reversedStr, 10);
+
+  return reversedNumber;
+};
